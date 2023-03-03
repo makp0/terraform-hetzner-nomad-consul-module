acbec56fc18034889326f8da80036dd3f6b3e9eb
Maksym Polupan <me@polupan.dev>
2023-03-03 20:12:20 +0700

    a

--- output.tf
+++ output.tf
@@ -7,20 +7,15 @@ output "server_info" {
   }
 }
 
-#data "local_file" "nomad_token" {
-#  depends_on = [null_resource.fetch_nomad_token]
-#  filename = "certs/nomad_token"
-#}
-
-#data "local_file" "nomad_token" {
-#  depends_on = [null_resource.fetch_nomad_token]
-#  filename = "certs/nomad_token"
-#}
+data "local_file" "nomad_token" {
+  depends_on = [null_resource.fetch_nomad_token]
+  filename = "${path.root}/certs/nomad_token"
+}
 
 output "nomad_token" {
-  #  depends_on = [data.local_file.nomad_token]
-  value = try(trimspace(file("${path.root}/certs/nomad_token")),  "Could not find nomad token file from initial bootstrap. If this is your initial apply, please create a GitHub issue.")
-  #  value = data.local_file.nomad_token.content
+  depends_on = [data.local_file.nomad_token]
+  #  value = try(trimspace(file("${path.root}/certs/nomad_token")),  "Could not find nomad token file from initial bootstrap. If this is your initial apply, please create a GitHub issue.")
+  value = data.local_file.nomad_token.content
 }
 
 output "nomad_address" {
